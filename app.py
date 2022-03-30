8b823be8a6c28160759f11f03f2d4614be8417cf
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-30 12:08:35 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,53 @@
-from flask import Flask, jsonify, redirect, render_template, request
-from flask_cors import CORS
-from werkzeug import exceptions
+from config import Config
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from random import choice
+import string
+from datetime import datetime
 
 
 app = Flask(__name__)
-CORS(app)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class ShortUrls(db.Model):
+    id  = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_id = db.Column(db.String(20), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        short_id = request.form['custom_id']
+
+        if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
+            flash('Please enter different custom id!')
+            return redirect(url_for('index'))
+
+        if not url:
+            flash('The URL is required!')
+            return redirect(url_for('index'))
+
+        if not short_id:
+            short_id = generate_short_id(8)
+
+        new_link = ShortUrls(
+            original_url=url, short_id=short_id, created_at=datetime.now())
+        db.session.add(new_link)
+        db.session.commit()
+        short_url = request.host_url + short_id
+
+        return render_template('index.html', short_url=short_url)
+
+    return render_template('index.html')
+
+def generate_short_id(num_of_chars):
+    """Function to generate short_id of specified number of characters"""
+    return ''.join(choice(string.ascii_lowercase+string.ascii_uppercase+string.digits) for _ in range(num_of_chars))
+
